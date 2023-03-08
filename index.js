436d153e0d8ea6601b689d2e4b3b8c6651e86868
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-08 13:43:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+// return shout(string)
+function shout (string) {
+    return string.toUpperCase();
+}
+shout (string)
+
+//return whisper(string)
+function whisper (string) {
+    return string.toLowerCase();
+} 
+whisper (string)
+
+//logShout(string)
+ function logShout (string) {
+    console.log(string.toUpperCase());
+ }
+ logShout (string)
+
+ //logWhisper(string)
+ function logWhisper (string) {
+    console.log(string.toLowerCase());
+ }
+ logWhisper (string)
+
+ //sayHiToHeadphonedRoommate(string)
+ function sayHiToHeadphonedRoommate (string) {
+    if (string===string.toLowerCase()) {
+        return "I can't hear you!";
+    }else if (string===string.toUpperCase()){
+        return "YES INDEED!";
+    }else if (string="Let's have dinner together!"){
+        return "I would love to!";
+    }
+ }
+ 
+ 
